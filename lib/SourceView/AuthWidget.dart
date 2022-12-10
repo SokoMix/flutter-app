6452daf36e217423342f9f55c483837f5b10f5e8
SokoMix <dimonsheu@email.ru>
2022-12-10 05:24:51 +0300

    Created CustomWidgets class. There are methods, which can generate widgets with style of app

--- lib/SourceView/AuthWidget.dart
+++ lib/SourceView/AuthWidget.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
+import '../CustomWidgets/CustomWidgets.dart';
 import 'Controller.dart';
-
-Widget _createAccount(context) {
-  return TextButton(
-      onPressed: () => {Navigator.of(context, rootNavigator: true).pushNamed('/new_acc')},
-      style: ButtonStyle(
-          alignment: Alignment.center,
-          fixedSize: MaterialStateProperty.all(Size(160, 40))),
-      child: Text(
-        "Создать аккаунт",
-        textAlign: TextAlign.center,
-        style: TextStyle(color: Color.fromRGBO(195, 98, 63, 1), fontSize: 15),
-      ));
-}
 
 class AuthView extends StatefulWidget {
   final Controller controller;
@@ -27,6 +15,7 @@ class _AuthViewState extends State<AuthView> {
   final Controller controller;
   final _login = TextEditingController();
   final _pswd = TextEditingController();
+  final customs = CustomWidgets();
   Image? logo = null;
 
   @override
@@ -62,110 +51,10 @@ class _AuthViewState extends State<AuthView> {
                   'Rush',
                   style: TextStyle(color: Color.fromRGBO(195, 98, 63, 1), fontSize: 50.0, decoration: TextDecoration.none, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic, fontFamily: 'Nexa'),
                 )),
-          Material(
-          color: Colors.black,
-          child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-              child: TextField(
-                cursorColor: Color.fromRGBO(195, 98, 63, 1),
-                style: TextStyle(color: Color.fromRGBO(195, 98, 63, 1)),
-                controller: _login,
-                autocorrect: false,
-                decoration: InputDecoration(
-                  icon: Icon(
-                    Icons.person,
-                    size: 20.0,
-                    color: Color.fromRGBO(195, 98, 63, 1),
-                  ),
-                  focusedErrorBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Color.fromRGBO(195, 98, 63, 1), width: 0.5),
-                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                  errorBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Color.fromRGBO(195, 98, 63, 1), width: 2.0),
-                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Color.fromRGBO(195, 98, 63, 1), width: 2.0),
-                      borderRadius: BorderRadius.all(Radius.circular(10.0))
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Color.fromRGBO(195, 98, 63, 1), width: 0.5),
-                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                  hintText: 'Логин',
-                  hintStyle: TextStyle(color: Color.fromRGBO(215, 121, 88, 1)),
-                ),
-              ))),
-            Material(
-                color: Colors.black,
-                child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-                    child: TextField(
-                      cursorColor: Color.fromRGBO(195, 98, 63, 1),
-                      style: TextStyle(color: Color.fromRGBO(195, 98, 63, 1)),
-                      controller: _pswd,
-                      obscureText: true,
-                      autocorrect: false,
-                      decoration: InputDecoration(
-                        icon: Icon(
-                          Icons.key,
-                          size: 20.0,
-                          color: Color.fromRGBO(195, 98, 63, 1),
-                        ),
-                        focusedErrorBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Color.fromRGBO(195, 98, 63, 1), width: 0.5),
-                            borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                        errorBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Color.fromRGBO(195, 98, 63, 1), width: 2.0),
-                            borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Color.fromRGBO(195, 98, 63, 1), width: 2.0),
-                            borderRadius: BorderRadius.all(Radius.circular(10.0))
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Color.fromRGBO(195, 98, 63, 1), width: 0.5),
-                            borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                        hintText: 'Пароль',
-                        hintStyle: TextStyle(color: Color.fromRGBO(215, 121, 88, 1)),
-                      ),
-                    ))),
-            Padding(
-                padding: EdgeInsets.only(top: 30.0, bottom: 15.0),
-                child: OutlinedButton(
-                    style: ButtonStyle(
-                      fixedSize: MaterialStateProperty.all(Size(120.0, 40.0)),
-                      backgroundColor: MaterialStateProperty.all(Color.fromRGBO(195, 98, 63, 1)),
-                      textStyle: MaterialStateProperty.all(
-                          TextStyle(fontSize: 21, fontWeight: FontWeight.bold)),
-                    ),
-                    onPressed: () {
-                      if (controller.loginUser(_login.text, _pswd.text)>-1) {Navigator.of(context, rootNavigator: true)
-                          .pushNamed('/home_page');}
-                      else {
-                        showDialog(context: context, builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Center(child: Text('Ошибка')),
-                            backgroundColor: Color.fromRGBO(195, 98, 63, 1),
-                            alignment: Alignment.center,
-                            content: Text('Неверно введен логин или пароль', textAlign: TextAlign.center,),
-                            actions: [
-                              TextButton(onPressed: () {Navigator.of(context).pop();}, child: Text('Закрыть', style: TextStyle(color: Color.fromRGBO(195, 98, 63, 1)),), style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.black)),)
-                            ],
-                          );
-                        });
-                      }
-                    },
-                    child: Text(
-                      'Войти',
-                      style: TextStyle(color: Colors.black),
-                    ))),
-            _createAccount(context),
+            customs.loginInput(_login),
+            customs.pswdInput(_pswd),
+            customs.loginBtn(context, controller, _login, _login),
+            customs.createAccountBtn(context)
           ],
         )));
   }
--- lib/SourceView/NewAccWidget.dart
+++ lib/SourceView/NewAccWidget.dart
@@ -1,5 +1,54 @@
 import 'package:flutter/material.dart';
+import '../CustomWidgets/CustomWidgets.dart';
 import 'Controller.dart';
+
+Function emailValidator = (str)
+{
+  if (str == null) {return 'Логин не может быть пустым';}
+  if (!RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+      .hasMatch(str)) {return 'Неверно указана почта';}
+  return null;
+};
+
+Function pswdValidator = (str)
+{
+  const String et_alph = 'abcdefghijklmnopqrstuvwxyz';
+  const String et_dig = '0123456789';
+  if (str == null) {return 'Пароль не может быть пустым';}
+  if (str.length < 6) {return 'Пароль должен включать хотя бы 6 символов';}
+  if (str.toLowerCase() == str) {return 'Пароль должен включать заглавную букву';}
+  if (str.toUpperCase() == str) {return 'Пароль должен включать строчную букву';}
+  int count_alph = 0, count_dig = 0;
+  str = str.toLowerCase();
+  for (int i = 0; i < str.length; i++) {
+    if (et_alph.contains(str[i])) {count_alph++;}
+    if (et_dig.contains(str[i])) {count_dig++;}
+  }
+  if (count_dig <= 0) {return 'Пароль должен включать хотя бы 1 цифру';}
+  if (count_alph <= 0) {return 'Пароль должен включать хотя бы 1 букву';}
+  if (count_dig+count_alph != str.length) {return 'Пароль должен состоять только из английских букв и цифр';}
+  return null;
+};
+
+Function nameValidator = (str) {
+  if ((str ?? '').isEmpty) return 'Имя не может быть пустым';
+  const String et = 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+  int count = 0;
+  str = str!.toLowerCase();
+  for (int i = 0; i < str.length; i++) count += et.contains(str[i]) ? 1 : 0;
+  if (count != str.length) return 'Имя должно включать только русские и английские буквы';
+  return null;
+};
+
+Function surnValidator = (str) {
+  if ((str ?? '').isEmpty) return 'Фамилия не может быть пустой';
+  const String et = 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+  int count = 0;
+  str = str!.toLowerCase();
+  for (int i = 0; i < str.length; i++) count += et.contains(str[i]) ? 1 : 0;
+  if (count != str.length) return 'Фамилия должна включать только русские и английские буквы';
+  return null;
+};
 
 class NewAccWidget extends StatefulWidget {
   final Controller controller;
@@ -10,19 +59,13 @@ class NewAccWidget extends StatefulWidget {
   State<NewAccWidget> createState() => _NewAccWidgetState(controller: controller);
 }
 
-String? checkEmail(int validate)
-{
-  if (validate == 1) {return 'Username Can\'t Be Empty';}
-  if (validate == 2) {return 'Incorrect email';}
-  return null;
-}
-
 class _NewAccWidgetState extends State<NewAccWidget> {
   final _formKey = GlobalKey<FormState>();
   final _login = TextEditingController();
   final _pswd = TextEditingController();
   final _fname = TextEditingController();
   final _sname = TextEditingController();
+  final customs = CustomWidgets();
   final Controller controller;
   _NewAccWidgetState({required this.controller});
   @override
@@ -35,172 +78,10 @@ class _NewAccWidgetState extends State<NewAccWidget> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-        Material(color: Colors.black,
-            child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-                child: TextFormField(
-                  controller: _login,
-                  autocorrect: false,
-                  cursorColor: Color.fromRGBO(195, 98, 63, 1),
-                  style: TextStyle(color: Color.fromRGBO(195, 98, 63, 1)),
-                  validator: (str)
-                  {
-                    if (str == null) {return 'Логин не может быть пустым';}
-                    if (!RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-                        .hasMatch(str)) {return 'Неверно указана почта';}
-                    return null;
-                  },
-                  decoration: InputDecoration(
-                    focusedErrorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(195, 98, 63, 1), width: 0.5),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                    errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(195, 98, 63, 1), width: 2.0),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(195, 98, 63, 1), width: 2.0),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Color.fromRGBO(195, 98, 63, 1), width: 0.5),
-                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                    hintStyle: TextStyle(color: Color.fromRGBO(195, 98, 63, 1)),
-                    hintText: 'Введите логин(адрес электронной почты)',
-                    // errorText: checkEmail(_validate),
-                  ),
-                )
-            )),
-    Material(color: Colors.black,
-        child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-                child: TextFormField(
-                  controller: _pswd,
-                  autocorrect: false,
-                  cursorColor: Color.fromRGBO(195, 98, 63, 1),
-                  style: TextStyle(color: Color.fromRGBO(195, 98, 63, 1)),
-                  validator: (str)
-                  {
-                    const String et_alph = 'abcdefghijklmnopqrstuvwxyz';
-                    const String et_dig = '0123456789';
-                    if (str == null) {return 'Пароль не может быть пустым';}
-                    if (str.length < 6) {return 'Пароль должен включать хотя бы 6 символов';}
-                    if (str.toLowerCase() == str) {return 'Пароль должен включать заглавную букву';}
-                    if (str.toUpperCase() == str) {return 'Пароль должен включать строчную букву';}
-                    int count_alph = 0, count_dig = 0;
-                    str = str.toLowerCase();
-                    for (int i = 0; i < str.length; i++) {
-                      if (et_alph.contains(str[i])) {count_alph++;}
-                      if (et_dig.contains(str[i])) {count_dig++;}
-                    }
-                    if (count_dig <= 0) {return 'Пароль должен включать хотя бы 1 цифру';}
-                    if (count_alph <= 0) {return 'Пароль должен включать хотя бы 1 букву';}
-                    if (count_dig+count_alph != str.length) {return 'Пароль должен состоять только из английских букв и цифр';}
-                    return null;
-                  },
-                  decoration: InputDecoration(
-                    focusedErrorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(195, 98, 63, 1), width: 0.5),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                    errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(195, 98, 63, 1), width: 2.0),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(195, 98, 63, 1), width: 2.0),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(195, 98, 63, 1), width: 0.5),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                    hintStyle: TextStyle(color: Color.fromRGBO(195, 98, 63, 1)),
-                    hintText: 'Введите пароль',
-                  ),
-                ))),
-            Material(color:Colors.black,
-                child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-                child: TextFormField(
-                  controller: _fname,
-                  autocorrect: false,
-                  cursorColor: Color.fromRGBO(195, 98, 63, 1),
-                  style: TextStyle(color: Color.fromRGBO(195, 98, 63, 1)),
-                  decoration: InputDecoration(
-                    focusedErrorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(195, 98, 63, 1), width: 0.5),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                    errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(195, 98, 63, 1), width: 2.0),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(195, 98, 63, 1), width: 2.0),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(195, 98, 63, 1), width: 0.5),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                    hintStyle: TextStyle(color: Color.fromRGBO(195, 98, 63, 1)),
-                    hintText: 'Введите имя',
-                  ),
-                  validator: (str) {
-                    if ((str ?? '').isEmpty) return 'Имя не может быть пустым';
-                    const String et = 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя';
-                    int count = 0;
-                    str = str!.toLowerCase();
-                    for (int i = 0; i < str.length; i++) count += et.contains(str[i]) ? 1 : 0;
-                    if (count != str.length) return 'Имя должно включать только русские и английские буквы';
-                    return null;
-                  },
-                ))),
-    Material(color:Colors.black,
-        child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-                child: TextFormField(
-                  controller: _sname,
-                  autocorrect: false,
-                  cursorColor: Color.fromRGBO(195, 98, 63, 1),
-                  style: TextStyle(color: Color.fromRGBO(195, 98, 63, 1)),
-                  decoration: InputDecoration(
-                    focusedErrorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(195, 98, 63, 1), width: 0.5),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                    errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(195, 98, 63, 1), width: 2.0),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(195, 98, 63, 1), width: 2.0),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(195, 98, 63, 1), width: 0.5),
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                    hintStyle: TextStyle(color: Color.fromRGBO(195, 98, 63, 1)),
-                    hintText: 'Введите фамилию',
-                  ),
-                  validator: (str) {
-                  if ((str ?? '').isEmpty) return 'Фамилия не может быть пустой';
-                  const String et = 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя';
-                  int count = 0;
-                  str = str!.toLowerCase();
-                  for (int i = 0; i < str.length; i++) count += et.contains(str[i]) ? 1 : 0;
-                  if (count != str.length) return 'Фамилия должна включать только русские и английские буквы';
-                  return null;
-                  },
-                ))),
+          customs.regInput(_login, emailValidator, 'Введите логин(адрес электронной почты)'),
+          customs.regInput(_pswd, pswdValidator, 'Введите пароль'),
+          customs.regInput(_fname, nameValidator, 'Введите имя'),
+          customs.regInput(_sname, surnValidator, 'Введите фамилию'),
     Padding(
                 padding: EdgeInsets.only(top: 30.0, bottom: 15.0),
                 child: OutlinedButton(
